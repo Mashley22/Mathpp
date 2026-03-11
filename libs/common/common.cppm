7d6247825ff04239ed0391798ef2a846dc63aa00
Ashley <ajkhsu22@gmail.com>
2026-03-11 18:28:13 +0000

    First real commit, initial repo structure setup

---
+++ libs/common/common.cppm
@@ -0,0 +1,3 @@
+export module Mathpp.common;
+
+export import :check;
---
+++ libs/common/include/Mathpp/macros.hpp
@@ -0,0 +1,8 @@
+#ifdef MATHPP_MACROS_HPP
+#define MATHPP_MACROS_HPP
+
+namespace mathpp {
+
+}
+
+#endif /* MATHPP_MACROS_HPP */
